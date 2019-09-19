f60f001fabd3d66561fd801dfea21b1fdc0efcc0
Eugene <tplrt901@gmail.com>
2019-09-19 15:30:05 +0300

    message

---
+++ ShortTask/binary.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void binary(int n) {
+	unsigned int mask;
+	int i = 0;
+	for (mask = ~((~0u)>>1); mask != 0; mask >>= 1) {
+		putchar(mask & (unsigned int) n ? '1' : '0');
+		i++;
+		if(!(i % 4) && mask != 1) {
+			putchar(i % 8 ? ' ' : '|');
+		}
+	}
+	putchar('\n');
+}
+
+int main() {
+	binary(666);
+}
