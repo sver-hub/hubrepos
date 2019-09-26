cba0f0d3f272e6f1874a9385ecce2d43b84f985f
Alexey Salnikov <alexey.salnikov@gmail.com>
2019-09-26 21:19:13 +0300

    Динамическая библиотека не работает, потому, что нужно прописать переменную окружения LD_LIBRARY_PATH или указывать полный путь до .so файла.
    В Makefile добавлено правило test, которое запускает пример с
    установленной переменной окружения LD_LIBRARY_PATH.
    
    По мелочи поправлено в исходниках.

--- shorttasks/plugin_matrix/matrix_pl.c
+++ shorttasks/plugin_matrix/matrix_pl.c
@@ -7,183 +7,206 @@
 
 typedef struct matrix
 {
-	int n;
-	double* p;
+    int n;
+    double* p;
 } matrix;
 
 int get_matrix(char* fname, matrix *mat) 
-{	
-	FILE* f;
-	int k;
-	char m[7] = {0,0,0,0,0,0,0};
-	void* tmp;
+{    
+    FILE* f;
+    int k;
+    char m[7] = {0,0,0,0,0,0,0};
+    void* tmp;
 
-	if((f = fopen(fname,"r")) == NULL) return 1;
+    if((f = fopen(fname,"r")) == NULL) return 1;
 
-	if(fread(&m, sizeof(char), 6, f)!= 6) 
-	{
-		fclose(f);
-		return 1;
-	}
+    if(fread(&m, sizeof(char), 6, f)!= 6) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	if(strcmp(m, "MATRIX")) 
-	{
-		fclose(f);
-		return 1;
-	}
+    if(strcmp(m, "MATRIX")) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	if(fread(&k, sizeof(int), 1, f) != 1) 
-	{
-		fclose(f);
-		return 1;
-	}
-	
-	mat->n = k;
-	tmp = (double*)malloc((k * k) * sizeof(double));
-	if(tmp == NULL) 
-	{
-		fclose(f);
-		return 1;
-	}
+    if(fread(&k, sizeof(int), 1, f) != 1) 
+    {
+        fclose(f);
+        return 1;
+    }
+    
+    mat->n = k;
+    tmp = (double*)malloc((k * k) * sizeof(double));
+    if(tmp == NULL) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	mat->p = tmp;
-	if(fread(mat->p, sizeof(double), k*k, f) != k*k) 
-	{
-		fclose(f);
-		return 1;
-	}
+    mat->p = tmp;
+    if(fread(mat->p, sizeof(double), k*k, f) != k*k) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	fclose(f);
-	return 0;
+    fclose(f);
+    return 0;
 }
 
 int put_matrix(char *fname, matrix *mat) 
 {
-	FILE *f;
-	int i, k = mat->n;
-	double d;
-	char* m = "MATRIX";
+    FILE *f;
+    int i, k = mat->n;
+    double d;
+    char* m = "MATRIX";
 
-	if((f = fopen(fname, "w")) == NULL) return 1;
+    if((f = fopen(fname, "w")) == NULL) return 1;
 
-	if(fwrite(m, sizeof(char),6, f) != 6) 
-	{
-		fclose(f);
-		return 1;
-	}
+    if(fwrite(m, sizeof(char),6, f) != 6) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	if(fwrite(&k, sizeof(int), 1, f) != 1) 
-	{
-		fclose(f);
-		return 1;
-	}
+    if(fwrite(&k, sizeof(int), 1, f) != 1) 
+    {
+        fclose(f);
+        return 1;
+    }
 
-	if(fwrite(mat->p, sizeof(double), (k * k), f) != (k * k))
-	{
-		fclose(f);
-		return 1;
-	}
-	
-	free(mat->p);
-	mat->n = 0;
-	mat->p = NULL;
-	fclose(f);
-	return 0;
+    if(fwrite(mat->p, sizeof(double), (k * k), f) != (k * k))
+    {
+        fclose(f);
+        return 1;
+    }
+    
+    free(mat->p);
+    mat->n = 0;
+    mat->p = NULL;
+    fclose(f);
+    return 0;
 }
 
 int create_random_matrix(matrix *mat)
 {
-	double d;
-	int i, m = mat->n;
-	void* tmp;
+    double d;
+    int i, m = mat->n;
+    void* tmp;
 
-	tmp = (double*)malloc((m * m)*sizeof(double));
-	if (tmp == NULL) return 1;
-	mat->p = tmp;
-	
-	for(i = 0; i < (m * m); i++)
-	{
-		d = (rand() + 0.)/rand();
-		mat->p[i] = d;
-	}
+    tmp = (double*)malloc((m * m)*sizeof(double));
+    if (tmp == NULL) return 1;
+    mat->p = tmp;
+    
+    for(i = 0; i < (m * m); i++)
+    {
+        d = (rand() + 0.)/rand();
+        mat->p[i] = d;
+    }
 
-	return 0;
+    return 0;
 }
 
 int print_matrix(matrix *mat)
 {
-	int  i, j, m = mat->n, k;
-	double d;
+    int  i, j, m = mat->n, k;
+    double d;
 
-	putchar('\n');
-	for(i = 0; i < m; i++)
-	{
-		putchar('|');
-		for(j = 0; j < m; j++)
-		{
-			k = i*m + j;
-			d = mat->p[k];
-			printf(" %8.3lf", d);
-		}
-		printf(" |\n");
-	}
-	return 0;
+    putchar('\n');
+    for(i = 0; i < m; i++)
+    {
+        putchar('|');
+        for(j = 0; j < m; j++)
+        {
+            k = i*m + j;
+            d = mat->p[k];
+            printf(" %8.3lf", d);
+        }
+        printf(" |\n");
+    }
+    return 0;
 }
 
-int main() 
+int main(int argc, char **argv) 
 {
-	char* fname = "file.bin";
-	int n = 0;
-	void* lib;
-        void (*func)(int *, double **)=NULL;
+    char* fname = "file.bin";
+    int n = 0;
+    void* lib=NULL;
+    void (*func)(int *, double **)=NULL;
 
-	matrix mat = {0, NULL};
-	srand(time(0));
+    if(argc<2)
+    {
+        fprintf(stderr, "We need name of .so file at first parameter of program\n");
+        return 1;
+    }
+    
+    /*
+     * The enviroment variable LD_LIBRARY_PATH is possible to set.
+     * But ld.so builds path to search before the main(int argc, char **argv) 
+     * function is called. 
+     */
+    /*
+    setenv("LD_LIBRARY_PATH", ".", 1);
+    */
 
-	scanf("%d", &n);
-	mat.n = n;
-	if(create_random_matrix(&mat))
-	{
-		printf("failed creating matrix\n");
-		return 0;
-	}
+    matrix mat = {0, NULL};
+    srand(time(0));
+    
+    printf("Please input matrix (NxN) size N: ");
+    scanf("%d", &n);
+    mat.n = n;
+    if(create_random_matrix(&mat))
+    {
+        printf("failed creating matrix\n");
+        return 0;
+    }
 
-	if(put_matrix(fname, &mat)) 
-	{	
-		printf("failed writing matrix to file\n");
-		return 0;
-	}
+    if(put_matrix(fname, &mat)) 
+    {    
+        printf("failed writing matrix to file\n");
+        return 0;
+    }
 
-	if(get_matrix(fname, &mat))
-	{
-		printf("failed reading from file\n");
-		return 0;
-	}
+    if(get_matrix(fname, &mat))
+    {
+        printf("failed reading from file\n");
+        return 0;
+    }
 
-	if(print_matrix(&mat))
-	{
-		printf("failed printing\n");
-		return 0;
-	}
-	
-	if((lib = dlopen("plugin.so",RTLD_LAZY)) == NULL) 
-	{
-		printf("lib not found\n");
-		return 0;
-	}
-	
-	func = dlsym(lib,"matrix_function");
-	if( func == NULL) 
-	{
-		printf("func not found\n");
-		return 0;
-	}
-	func(&mat.n,&mat.p);
-	dlclose(lib);
+    if(print_matrix(&mat))
+    {
+        printf("failed printing\n");
+        return 0;
+    }
 
-	print_matrix(&mat);
+    lib = dlopen(argv[1], RTLD_LAZY);
+    if( lib == NULL) 
+    {
+        printf("lib not found - %s\n", dlerror());
+        return 1;
+    }
+    
+    func = dlsym(lib,"matrix_function");
+    if( func == NULL) 
+    {
+        printf("func not found\n");
+        return 0;
+    }
+    
+    /*
+     * Now we do matrix filtration by function
+     * extracted from  .so file.
+     */
+    func(&mat.n,&mat.p);
+    
+    dlclose(lib);
 
-	free(mat.p);
-	return 0;
+    print_matrix(&mat);
+
+    free(mat.p);
+    return 0;
 }
 
