21ede1d1994ac748f764334903ce567d936284ff
Eugene <tplrt901@gmail.com>
2019-09-28 11:53:33 +0300

    task 7 completed

--- shorttasks/commets_7/comments.c
+++ shorttasks/commets_7/comments.c
@@ -1,133 +1,180 @@
-#define BUFFADD 100
-#define ERROR_MEM -1
-#define EOFFF -2
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/types.h>
 
+#define BUFFADD 50
 
-ssize_t read_str(FILE* f, char** strp) 
+#define MEM_ERROR -1
+
+ssize_t fread_line(FILE* f, char** line, int* e)
 {
-	char *buf = NULL;
-	size_t size = 0;
-	size_t mem = 0;
-	char *tmp = NULL;
-	int sym = 0;
+    char* buff = NULL;
+    size_t size = 0;
+    char* tmp = NULL;
+    size_t mem = 0;
+    int sym = 0;
+    *e = 0;
 
-	while(1)
-	{
-		sym = fgetc(f);
-		if (sym == '\n' || sym == EOF) 
-			{
-				if(!size) return EOFFF;
-				break;
-			}
+    while(1)
+    {
+        sym = fgetc(f);
+        if(sym == '\n' || (sym == EOF && (*e = 1))) break;
 
-		if (size == mem)
-		{
-			tmp = (char*)realloc(buf, size + BUFFADD);
-			if (tmp == NULL) return ERROR_MEM;
-			buf = tmp;
-			mem += BUFFADD;
-		}
-		buf[size] = (char) sym;
-		size++;
-	}
-	
-	tmp = (char*)realloc(buf, size + 1);
-	if (tmp == NULL) return ERROR_MEM;
-	buf = tmp;
-	buf[size] = '\0';
-	*strp = buf;
-	return size;
+        if(size == mem)
+        {
+            tmp = (char*) realloc(buff, (size + BUFFADD) * sizeof(char));
+            if(tmp == NULL)
+            {
+                return MEM_ERROR; 
+            }
+
+            buff = tmp;
+            mem += BUFFADD;
+        }
+        buff[size++] = (char)sym;
+    }
+
+    tmp = (char*) realloc(buff, (size + 1) * sizeof(char));
+    if(tmp == NULL) return MEM_ERROR;
+
+    buff = tmp;
+    buff[size] = '\0';
+    *line = buff;
+
+    return size;
 }
 
-int delcom(char* fname)
+ssize_t rd_lines(FILE* f, char*** lines)
 {
-	FILE *f;
-	char *str, *tmp, *towrite = NULL;
-	int i, k, quotes = 0, len, alllen = 0;
+    char** buff = NULL, **tmp = NULL;
+    size_t size = 0, mem = 0;
+    char* line;
+    int e;
 
-	if((f = fopen(fname, "r")) == NULL) return 1;
+    while(1)
+    {
+        if(fread_line(f, &line, &e) < 0) break;
 
-	while(1)
-	{
-		len = read_str(f, &str);
-		if (len == ERROR_MEM) 
-		{
-			printf("error\n");
-			fclose(f);
-			return 1;
-		}
-		else if (len == EOFFF) break;
+        if(size == mem)
+        {
+            tmp = (char**) realloc(buff, (size + BUFFADD) * sizeof(char*));
+            if(tmp == NULL) return MEM_ERROR;
+            buff = tmp;
+            mem += BUFFADD;
+        }
 
-		for (i = 0; i < len; i++)
-		{	
-			if (!quotes && str[i] == '\"') quotes = 1;
-			else if (quotes && str[i] == '\"') quotes = 0;
+        buff[size++] = line;
+        if(e) break;
+    }
 
-			if (str[i] == '/' && str[i+1] == '/' && !quotes) break;
-		}
+    tmp = (char**) realloc(buff, (size + 1) * sizeof(char*));
+    if(tmp == NULL) return MEM_ERROR;
 
-		if(i)
-		{
-			tmp = (char*)realloc(towrite, (i + 1 + alllen)*sizeof(char));
-			if (tmp == NULL) 
-				{
-					fclose(f);
-					return 1;
-				}
-			for (k = 0; k < i; k ++) tmp[k + alllen] = str[k];
+    buff = tmp;
+    buff[size] = NULL;
+    *lines = buff;
 
-			free(str);
-			tmp[i + alllen] = '\n';
-			alllen += (i + 1);
-			towrite = tmp;
-		}
-	}
-	towrite[alllen - 1] = '\0';
+    return size;
+}
 
-	if ((f = fopen(fname, "w")) == NULL) return 1;
-	fputs(towrite, f);
-	fclose(f);
+int cmp_lines(const void* p1, const void* p2)
+{
+    return strcmp(*(char* const*)p1, *(char* const*)p2);
+}
 
-	return 0;
+
+ssize_t rm_comments(char** line)
+{
+    int in_string = 0;
+    size_t len;
+
+    char* tmp = NULL;
+
+    for(len = 0; line[0][len] != '\0'; len++)
+    {
+        line[0][len] == '\"' && (in_string = (in_string ? 0 : 1));
+
+        if(!in_string && line[0][len] == '/' && line[0][len + 1] == '/') break;
+    }
+
+    tmp = (char*) realloc(*line, (len + 1) * sizeof(char));
+    if(tmp == NULL) return MEM_ERROR;
+
+    tmp[len] = '\0';
+
+    *line = tmp;
+    
+    return len;
 }
 
 int write_file(FILE* f)
 {
-	char* text = "here is some //text bruh\nand then \"some //more\"\nand again// text\n//comment";
+    char* text = "here is some //text \n\
+and then \"some //more\"\n\
+and again// text\n\
+//comment\n\
+testing out// the prog";
 
-	if (fputs(text, f) == EOF) return 1;
-	return 0;
+    if (fputs(text, f) == EOF) return 1;
+    return 0;
 }
 
-int main() 
+
+int main(int argc, char* argv[])
 {
-	FILE* f;
-	char* fname = "test.txt";
+    FILE* f;
 
-	if((f = fopen(fname, "w")) == NULL)
+    char** lines = NULL;
+
+    size_t size, i, len;
+
+	if(argc < 2)
 	{
-		printf("couldn open file\n");
+		printf("Not enough arguments\n");
 		return 1;
 	}
 
-	if(write_file(f)) 
-	{
-		printf("couldn`t write file\n");
-		return 1;
-	}
+    f = fopen(argv[1], "w");
+    write_file(f);
+    fclose(f);
+
+	f = fopen(argv[1], "r");
+
+    size = rd_lines(f, &lines);
+    
+    printf("\n\n___ORIGINAL_FILE___\n");
+    for(i = 0; i < size; ++i)
+    {
+        printf("%s\n", lines[i]);
+    }
+
+    qsort(lines, size, sizeof(char*), cmp_lines);
+
+    fclose(f);
+    f = fopen(argv[1], "w");
+
+    printf("\n\n___EDITED_FILE___\n");
+    for(i = 0; i < size; ++i)
+    {
+        len = rm_comments(&lines[i]);
+        if (len)
+        {
+            fputs(lines[i], f); 
+            fputc('\n', f);
+
+            printf("%s\n", lines[i]);
+        }
+    }
+    printf("\n\n");
+
+    for(i = 0; i < size; ++i)
+    {
+        free(lines[i]);
+    }
+    free(lines);
 
 	fclose(f);
-
-	if(delcom(fname))
-	{
-		printf("error occured during comment deletion\n");
-		return 1;
-	}
 	return 0;
 }
-
-	
-
